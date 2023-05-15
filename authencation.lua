8e83e591b31ef663073f184cbdf172d0df7b2700
mrlokiie123 <133542179+mrlokiie123@users.noreply.github.com>
2023-05-15 14:34:48 -0400

    Update authencation.lua

--- authencation.lua
+++ authencation.lua
@@ -1,1 +1,9 @@
+local Players = game:GetService('Players');
 
+local KEY_AUTHENCATION = {
+    "MRLOKIIE:cXBSBkl*&$01-x_xncBX71XJZKKXNCZNXC12NX",
+};
+
+game.Players.PlayerAdded:Connect(function(Player) 
+    print(Player.Name .. ', has joined the game!');
+end)
